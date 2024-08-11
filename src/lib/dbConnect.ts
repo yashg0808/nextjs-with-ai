5e38a75241b843350c22be3ee125885c73d93d3e
Yash Gupta <yash.gupta0808@gmail.com>
2024-08-11 18:51:29 +0530

    add email functionality

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database.");
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Connected to database.");
+  } catch (err) {
+    console.log("Error connecting to database: ", err);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
